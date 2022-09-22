3dcc6a95ad32775dede245b27ca0c127f566441c
yaldy <natalia.vegaq@usm.cl>
2022-09-22 11:38:36 -0700

    leer.c-> paso por valor, main.c -> pipes

--- Tarea1/leer.c
+++ Tarea1/leer.c
@@ -9,7 +9,7 @@ typedef struct{
   int tiempo;
 }producto;
 
-int main()
+int leer_prod() //paramatros campo(),lo que se busca
 {
   FILE* ptr; // se declara puntero para arreglo donde se guarda lo que esta en el .txt
   char ch;
@@ -23,27 +23,33 @@ int main()
     printf("file can't be opened \n");
   }
 
-  // se leen la linea del archivo de texto
+  // se lee linea a linea el archivo de texto
   while(!feof(ptr)){
     ch = fgetc(ptr); // lee un caracter del archivo de texto
     texto[a] = ch;
-    if(texto[a] == '\n'){ // si hay salto de linea, se sale del while
+    if(texto[a] == '\n'){ 
       a = -1;
       // se guardan los datos ordenados en un struct
       s = strtok(texto, ",");
-      memset(Pprod->descripcion,'\0',sizeof(Pprod->descripcion)); // rellena descripcion con caracteres nulos
-      strcpy(Pprod->descripcion,s); // iguala dos string (string copy)
+      memset(prod.descripcion,'\0',sizeof(prod.descripcion)); // rellena descripcion con caracteres nulos
+      strcpy(prod.descripcion,s); // iguala dos string (string copy)
       int n = 0;
       while(s != NULL){
         s = strtok(NULL,",");
         switch(n){ // guarda en el struct
-          case 0: Pprod->precio = atoi(s);
-          case 1: Pprod->codigo = atoi(s);
-          case 2: Pprod->tiempo = atoi(s);
+          case 0: prod.precio = atoi(s);
+          case 1: prod.codigo = atoi(s);
+          case 2: prod.tiempo = atoi(s);
         }
         n++;
       }
-    printf("%s\n", Pprod->descripcion);
+	  
+	//comparar para encontrar lo que se busca (descripción o codigo)
+	
+	switch(){
+		
+	}
+    printf("%s\n", prod.descripcion);
     }
     a++;
   }
